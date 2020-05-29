91dae4b44d616152bb40288a427a2fce01ffbe5c
Tolga Üstünkök <tolgaustunkok@gmail.com>
2020-05-29 11:02:55 +0300

    Passwords and usernames are hidden inside environment variables.

--- se_mudek_mng_project/settings.py
+++ se_mudek_mng_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%h292q8zs1^)p757)v#8*952txwnzcuviw8tynxxk+ewxp6%()'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
