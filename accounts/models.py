714ffab6fdd74c3cfe720413e3c1c5c3fcbe8c81
Tolga Üstünkök <tolgaustunkok@gmail.com>
2021-02-22 00:14:04 +0300

    Update v1.0.0-rc8: Small bug fixes are made to enabling maintenance mode.

--- accounts/models.py
+++ accounts/models.py
@@ -6,8 +6,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
